b1d0cc1729f81d3d366bbf8b92505c9f579d78c5
Viktor Edman <viktor@edman.se>
2026-05-26 09:30:27 +0200

    Added tmux powerline

--- home.nix
+++ home.nix
@@ -4,6 +4,12 @@
   home.username = "viktor";
   home.homeDirectory = "/home/viktor";
   home.stateVersion = "24.11";
+  home.file.".config/tmux-powerline/config.sh".text = ''
+    export TMUX_POWERLINE_LEFT_STATUS_SEGMENTS="session_info hostname"
+    export TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS="lan_ip wan_ip date_time"
+    export TMUX_POWERLINE=DATE_FORMAT="%F"
+    export TMUX_POWERLINE=TIME_FORMAT="%H:%M"
+  '';
   programs.tmux = {
     enable = true;
     baseIndex = 1;
@@ -14,18 +20,16 @@
     package = pkgs.tmux;
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
-      sensible
       better-mouse-mode
+      tmux-powerline
     ];
     extraConfig = ''
       set-option -g renumber-windows on
       set-option -sa terminal-features ',tmux-256color:RGB'
-      set-option -g set-clibpoard on
-
+      set-option -g set-clipboard on
       bind '"' split-window -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
-
       set -g @catppuccin_flavour 'frappe'
     '';
   };
